f49debd03d6493910c033c1b4cbb7f85f3168abd
Quintin Smith <quintin@getweave.com>
2015-12-23 19:52:54 -0700

    I added a function

--- starter.py
+++ starter.py
@@ -5,3 +5,16 @@ print me
 print "I am excited for this"
 
 print "Yea buddy!"
+
+
+
+#this is what a function looks like
+#this takes the string up top and makes it uppercase
+
+def makeStringUppercase(str):
+    print str.upper()
+    return
+
+#this is how I call a function
+
+makeStringUppercase(me)
