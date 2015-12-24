f93af1f272a7dee0f2fc67ee1a383580272974be
palelei72 <palelei72@gmail.com>
2015-12-23 18:23:49 -0700

    I need help with this large large file

---
+++ phrase_exercise.py
@@ -0,0 +1,15 @@
+Python 2.7.10 (default, Oct 14 2015, 16:09:02) 
+[GCC 5.2.1 20151010] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print dookie
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    print dookie
+NameError: name 'dookie' is not defined
+>>> print "dookie"
+dookie
+>>> phrase = "I wish you would try to eat my snow cone"
+>>> print phrase
+I wish you would try to eat my snow cone
+>>> 
